9502596e0092011ed97ccf1ff0db1da8282bb2f2
SergeyTon <ct6702@gmail.com>
2023-01-14 21:14:34 +0300

    fixed profession

--- init.js
+++ init.js
@@ -6,7 +6,7 @@ window.onload = function () {
     document.querySelector('#surnameOutput').innerText = initPerson.surname;
     document.querySelector('#middleNameOutput').innerText = initPerson.middlename;
     document.querySelector('#birthYearOutput').innerText = initPerson.birthDates;
-    document.querySelector('#professionsOutput').innerText = initPerson.professions;
+    document.querySelector('#professionOutput').innerText = initPerson.profession;
 };
 
 document.querySelector('#next').onclick = window.onload;
@@ -17,5 +17,5 @@ document.querySelector('#clear').onclick = function () {
     document.querySelector('#middleNameOutput').innerText = "";
     document.querySelector('#genderOutput').innerText = "";
     document.querySelector('#birthYearOutput').innerText = "";
-    document.querySelector('#professionsOutput').innerText = "";
+    document.querySelector('#professionOutput').innerText = "";
 }
